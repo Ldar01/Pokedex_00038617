1d03f5318db576f52fcc95aca95aa45e85c8618f
Luisa Arevalo <00038617@uca.edu.sv>
2019-04-04 10:41:29 -0600

    Pruebas

--- app/src/main/java/com/example/tarea_nextor/PokemonAdapter.kt
+++ app/src/main/java/com/example/tarea_nextor/PokemonAdapter.kt
@@ -8,16 +8,16 @@ import android.widget.TextView
 import com.example.tarea_nextor.Utils.Pokemon
 import kotlinx.android.synthetic.main.pokemon_list2.view.*
 
-class PokemonAdapter(val items: List<Pokemon>) : RecyclerView.Adapter<PokemonAdapter.ViewHolder>() {
+class PokemonAdapter(var items: List<Pokemon>) : RecyclerView.Adapter<PokemonAdapter.ViewHolder>() {
 
-    private var countViews: Int = 0
+    //private var countViews: Int = 0
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.pokemon_list2, parent, false)
 
-        view.findViewById<TextView>(R.id.pokemon_id_count).text = countViews.toString()
-        countViews++
+       // view.findViewById<TextView>(R.id.pokemon_id_count_recycle).text = countViews.toString()
+       // countViews++
         return ViewHolder(view)
     }
 
@@ -33,9 +33,9 @@ class PokemonAdapter(val items: List<Pokemon>) : RecyclerView.Adapter<PokemonAda
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         fun bind(item: Pokemon) = with(itemView) {
-            nombre_pokemon.text = item.name
-            tipo_pokemon.text = item.types.toString()
-            others.text = item.abilities.toString()
+            //pokemon_id_count_recycle.text = item.id.toString()
+            nombre_pokemon_recycle.text = item.name
+
         }
     }
 }
--- app/src/main/java/com/example/tarea_nextor/Utils/NetworkConnection.kt
+++ app/src/main/java/com/example/tarea_nextor/Utils/NetworkConnection.kt
@@ -16,11 +16,10 @@ object NetworkUtils {
 
     private val TAG = NetworkUtils::class.java.simpleName
 
-    fun buildUrl(pokeID: String): URL? {
+    fun buildUrl(): URL? {
         val builtUri = Uri.parse(POKEMON_API_BASE_URL)
             .buildUpon()
             .appendPath(POKEMON_INFO)
-            .appendPath(pokeID)
             .build()
 
         var url: URL? = null
